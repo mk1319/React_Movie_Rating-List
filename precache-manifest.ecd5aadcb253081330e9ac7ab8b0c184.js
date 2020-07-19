11a4a8912f9db0f7c0cbe180c4b39838f0799051
mk1319 <mukeshrajpurohit88977@gmail.com>
2020-07-19 18:15:30 +0530

    Updates

---
+++ precache-manifest.ecd5aadcb253081330e9ac7ab8b0c184.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6fa829c6a791adf70fd0e0b900ebba3",
+    "url": "/MovieList/index.html"
+  },
+  {
+    "revision": "37458a0fc037ad14dfce",
+    "url": "/MovieList/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b085aa093eafbc2ef2f5",
+    "url": "/MovieList/static/css/main.da307a7b.chunk.css"
+  },
+  {
+    "revision": "37458a0fc037ad14dfce",
+    "url": "/MovieList/static/js/2.e89fd0c0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/MovieList/static/js/2.e89fd0c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b085aa093eafbc2ef2f5",
+    "url": "/MovieList/static/js/main.4120c37e.chunk.js"
+  },
+  {
+    "revision": "72d47e7bcc6df74dcd2f",
+    "url": "/MovieList/static/js/runtime-main.626134cf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MovieList/static/media/logo.5d5d9eef.svg"
+  }
+]);
